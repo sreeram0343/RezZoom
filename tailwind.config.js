5deaedf18887e2e63d293f8251ecf83e0919cfe2
sreeram0343 <sreerammurali2005@gmail.com>
2026-04-13 21:42:09 +0530

    Update entire color combo to #EDF1F5 and #0145F2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          50: '#EDF1F5',
+        },
+        blue: {
+          500: '#0145F2',
+          600: '#0138c6',
+        }
+      }
+    },
   },
   plugins: [],
 }
